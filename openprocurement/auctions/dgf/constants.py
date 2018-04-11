9e54438dd67985ab666309498b7ff946f270b034
Andrew Leitsius <leits@quintagroup.com>
2018-04-11 14:43:18 +0300

    Remove unused view location

--- openprocurement/auctions/dgf/constants.py
+++ openprocurement/auctions/dgf/constants.py
@@ -5,12 +5,10 @@ from openprocurement.api.models import TZ
 
 FINANCIAL_VIEW_LOCATIONS = [
     "openprocurement.auctions.dgf.views.financial",
-    "openprocurement.auctions.core.plugins",
 ]
 
 OTHER_VIEW_LOCATIONS = [
     "openprocurement.auctions.dgf.views.other",
-    "openprocurement.auctions.core.plugins",
 ]
 
 DGF_ID_REQUIRED_FROM = datetime(2017, 1, 1, tzinfo=TZ)
