05fdadbdc51b743b3ffb0ce906dc9e6639155bd1
vitalii-motso <vitalii.motso@gmail.com>
2018-05-04 20:03:17 +0300

    fix import error: uuid

--- openprocurement/auctions/dgf/tests/migration.py
+++ openprocurement/auctions/dgf/tests/migration.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
-
+from uuid import uuid4
 from openprocurement.auctions.core.utils import get_now
 from openprocurement.auctions.core.tests.base import snitch
 from openprocurement.auctions.core.tests.blanks.migration_blanks import migrate
