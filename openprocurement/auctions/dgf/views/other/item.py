7e47888b928ea7aab077a2bdea8e0e0f9c4f30e8
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-08-20 17:50:53 +0300

    Finish item's POST view

--- openprocurement/auctions/dgf/views/other/item.py
+++ openprocurement/auctions/dgf/views/other/item.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from openprocurement.auctions.core.utils import (
     apply_patch,
+    context_unpack,
     get_now,
     json_view,
     opresource,
@@ -28,3 +29,20 @@ class AuctionLotResource(AuctionLotResource):
     def collection_post(self):
         item = self.request.validated['item']
         self.context.items.append(item)
+        if save_auction(self.request):
+            self.LOGGER.info(
+                'Created lot item {}'.format(item.id),
+                extra=context_unpack(
+                    self.request,
+                    {'MESSAGE_ID': 'auction_item_create'},
+                    {'item_id': item.id}
+                )
+            )
+            self.request.response.status = 201
+            item_route = self.request.matched_route.name.replace("collection_", "")
+            self.request.response.headers['Location'] = self.request.current_route_url(
+                _route_name=item_route,
+                item_id=item.id,
+                _query={}
+            )
+            return {'data': item.serialize("view")}
