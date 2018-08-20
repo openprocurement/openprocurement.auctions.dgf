059325db7f8a2a9cc5a67019f5228dd9a6a77701
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-08-20 14:51:25 +0300

    Stub view for Item

---
+++ openprocurement/auctions/dgf/views/other/item.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from openprocurement.auctions.core.utils import (
+    apply_patch,
+    get_now,
+    json_view,
+    opresource,
+    save_auction,
+)
+from openprocurement.auctions.core.views.mixins import AuctionLotResource
+from openprocurement.auctions.core.endpoints import ENDPOINTS
+
+
+@opresource(name='dgfOtherAssets:Auction Items',
+            collection_path=ENDPOINTS['items'],
+            path=ENDPOINTS['item'],
+            auctionsprocurementMethodType="dgfOtherAssets",
+            description="Auction items")
+class AuctionLotResource(AuctionLotResource):
+
+    @json_view(content_type="application/json", permission='edit_auction')
+    def collection_post(self):
+        pass
