1d020a04cf6302498c3426e60d46c2ce00e5d878
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-08-23 16:28:52 +0300

    Add test for the dgfFinancial items resource

--- openprocurement/auctions/dgf/tests/item.py
+++ openprocurement/auctions/dgf/tests/item.py
@@ -3,6 +3,7 @@ import unittest
 
 from openprocurement.auctions.dgf.tests.base import (
     BaseAuctionWebTest,
+    test_financial_auction_data,
 )
 from openprocurement.auctions.core.tests.items import (
     DgfItemsResourceTestMixin,
@@ -13,9 +14,15 @@ class DgfOtherItemsResourceTest(BaseAuctionWebTest, DgfItemsResourceTestMixin):
     initial_status = 'active.tendering'
 
 
+class DgfFinancialItemsResourceTest(BaseAuctionWebTest, DgfItemsResourceTestMixin):
+    initial_status = 'active.tendering'
+    initial_data = test_financial_auction_data
+
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(DgfOtherItemsResourceTest))
+    suite.addTest(unittest.makeSuite(DgfFinancialItemsResourceTest))
 
     return suite
 
