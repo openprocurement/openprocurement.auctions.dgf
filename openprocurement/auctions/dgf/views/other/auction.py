f437a406f14205f154980effb116cfde55cbe552
Bohdan Dmytriv <bdmbdsm@users.noreply.github.com>
2018-09-25 15:15:24 +0300

    Use `pendify_auction_status` (#118)

--- openprocurement/auctions/dgf/views/other/auction.py
+++ openprocurement/auctions/dgf/views/other/auction.py
@@ -13,6 +13,7 @@ from openprocurement.auctions.core.validation import (
 from openprocurement.auctions.core.views.mixins import AuctionAuctionResource
 
 from openprocurement.auctions.core.plugins.awarding.base.utils import invalidate_bids_under_threshold
+from openprocurement.auctions.core.interfaces import IAuctionManager
 
 
 @opresource(name='dgfOtherAssets:Auction Auction',
@@ -26,11 +27,12 @@ class AuctionAuctionResource(AuctionAuctionResource):
     def collection_post(self):
         apply_patch(self.request, save=False, src=self.request.validated['auction_src'])
         auction = self.request.validated['auction']
+        adapter = self.request.registry.getAdapter(auction, IAuctionManager)
         invalidate_bids_under_threshold(auction)
         if any([i.status == 'active' for i in auction.bids]):
             self.request.content_configurator.start_awarding()
         else:
-            auction.status = 'unsuccessful'
+            adapter.pendify_auction_status('unsuccessful')
         if save_auction(self.request):
             self.LOGGER.info('Report auction results', extra=context_unpack(self.request, {'MESSAGE_ID': 'auction_auction_post'}))
             return {'data': self.request.validated['auction'].serialize(self.request.validated['auction'].status)}
@@ -41,13 +43,14 @@ class AuctionAuctionResource(AuctionAuctionResource):
         """
         apply_patch(self.request, save=False, src=self.request.validated['auction_src'])
         auction = self.request.validated['auction']
+        adapter = self.request.registry.getAdapter(auction, IAuctionManager)
         if all([i.auctionPeriod and i.auctionPeriod.endDate for i in auction.lots if i.numberOfBids > 1 and i.status == 'active']):
             cleanup_bids_for_cancelled_lots(auction)
             invalidate_bids_under_threshold(auction)
             if any([i.status == 'active' for i in auction.bids]):
                 self.request.content_configurator.start_awarding()
             else:
-                auction.status = 'unsuccessful'
+                adapter.pendify_auction_status('unsuccessful')
         if save_auction(self.request):
             self.LOGGER.info('Report auction results', extra=context_unpack(self.request, {'MESSAGE_ID': 'auction_lot_auction_post'}))
             return {'data': self.request.validated['auction'].serialize(self.request.validated['auction'].status)}
